d3fe80ef488e2d7c5b767308ec14a019887a57db
jason p <contactfullmetal@gmail.com>
2020-08-02 20:37:19 -0500

    working with local wordpress install

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,26 +10,26 @@ module.exports = {
      * Gatsby's data processing layer begins with “source”
      * plugins. Here the site sources its data from WordPress.
      */
+
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        /*
-         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
-         * Example : 'demo.wp-api.org' or 'www.example-site.com'
-         */
-        baseUrl: `live-gatbsyjswp.pantheonsite.io`,
-        // The protocol. This can be http or https.
-        protocol: `https`,
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
-        hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
-        // This feature is untested for sites hosted on WordPress.com
-        useACF: true,
-      },
-    },
+          // Specify the URL of the WordPress source
+          baseUrl: `localhost/gatsbywp/wordpress`,
+          protocol: `http`,
+          // Indicates if a site is hosted on WordPress.com
+          hostingWPCOM: false,
+          // Specify which URL structures to fetch
+          includedRoutes: [
+            '**/posts',
+            '**/tags',
+            '**/categories'
+          ]
+      }
+  },
+  
+
+
     /**
      * The following plugins aren't required for gatsby-source-wordpress,
      * but we need them so the default starter we installed above will keep working.
